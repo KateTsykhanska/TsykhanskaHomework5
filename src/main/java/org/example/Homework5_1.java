add49e87cad9a43f3e1dfa96b8c4a6d3845b9264
Katia <kateeegold+1@gmail.com>
2024-10-29 11:29:19 +0100

    commit

---
+++ src/main/java/org/example/Homework5_1.java
@@ -0,0 +1,14 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Homework5_1 {
+    public static void checkAge (int age) {
+        if (age < 18) {
+            System.out.println("You are not adult");
+        }
+        else {
+            System.out.println("You are adult");
+        }
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+
+    package org.example;
+
+    public class Main {
+        public static void main (String[] args) {
+            Homework5_1 homework5_1 = new Homework5_1();
+            homework5_1.checkAge(9);
+            homework5_1.checkAge(10);
+            homework5_1.checkAge(0);
+            homework5_1.checkAge(99);
+            homework5_1.checkAge(35);
+            homework5_1.checkAge(25);
+        }
+}
