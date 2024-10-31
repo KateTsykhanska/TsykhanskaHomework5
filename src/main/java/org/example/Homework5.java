fe06975745b1b79e511a3859cd94b001aa245f44
Katia <kateeegold+1@gmail.com>
2024-10-31 09:09:28 +0100

    5.3

--- src/main/java/org/example/Homework5.java
+++ src/main/java/org/example/Homework5.java
@@ -1,8 +1,10 @@
 package org.example;
 
+import java.sql.SQLOutput;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Homework5_1 {
+public class Homework5 {
     public static void checkAge (int age) {
         if (age < 18) {
             System.out.println("You are not adult");
@@ -15,6 +17,15 @@ public class Homework5_1 {
         if (number < 0){return false;
         }
         return (int)Math.sqrt(number)*Math.sqrt(number) == number;
+
+    }
+
+    public void sumUpToN (int n) {
+        int result = 0;
+        for(int i = 1; i<=n; i++) {
+            result += i;
+        }
+        System.out.println("Sum = " + result);
 
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@
 
     public class Main {
         public static void main (String[] args) {
-            Homework5_1 homework5_1 = new Homework5_1();
+            Homework5 homework5_1 = new Homework5();
             homework5_1.checkAge(9);
             homework5_1.checkAge(10);
             homework5_1.checkAge(0);
@@ -14,5 +14,6 @@
             System.out.println("Homework.isPerfectSquare (25): "+homework5_1.isPerfectSquare(25));
             System.out.println("Homework.isPerfectSquare (24): "+homework5_1.isPerfectSquare(24));
 
+            homework5_1.sumUpToN(8);
         }
 }
