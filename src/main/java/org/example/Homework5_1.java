ea995bcd9f66a32c0864d64d5e2bf604d98debb2
Katia <kateeegold+1@gmail.com>
2024-11-12 22:09:01 +0100

    formatting

--- src/main/java/org/example/Homework5_1.java
+++ src/main/java/org/example/Homework5_1.java
@@ -3,21 +3,22 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Homework5_1 {
-    public static void checkAge (int age) {
+    public static void checkAge(int age) {
         if (age < 18) {
             System.out.println("You are not adult");
-        }
-        else {
+        } else {
             System.out.println("You are adult");
         }
     }
-    public  boolean isPerfectSquare (int number) {
-        if (number < 0){return false;
+
+    public boolean isPerfectSquare(int number) {
+        if (number < 0) {
+            return false;
         }
-        return (int)Math.sqrt(number)*Math.sqrt(number) == number;
+        return (int) Math.sqrt(number) * Math.sqrt(number) == number;
     }
 
-    public static void getGrade (int note) {
+    public static void getGrade(int note) {
         if (note == 5) {
             System.out.println("Excellent");
         } else if (note == 1) {
@@ -25,7 +26,7 @@ public class Homework5_1 {
         } else if (note < 0) {
             System.out.println("Wrong score");
         }
-
-
-        }
     }
+
+
+}
