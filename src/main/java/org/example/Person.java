7da35044b11ff638684e098cceb62e602984be4c
Katia <kateeegold+1@gmail.com>
2024-11-18 15:00:13 +0100

    formatting

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -6,32 +6,41 @@ public class Person {
     String name;
     int age;
     String job;
+
     Person(String name, int age, String job) {
         this.name = name;
         this.age = age;
         this.job = job;
     }
-    void setName(String name){
+
+    void setName(String name) {
         this.name = name;
     }
+
     void setAge(int age) {
         this.age = age;
     }
-    void setJob(String job){
+
+    void setJob(String job) {
         this.job = job;
     }
-    String getName(){
+
+    String getName() {
         return name;
     }
-    int getAge(){
+
+    int getAge() {
         return age;
     }
-    String getJob(){
+
+    String getJob() {
         return job;
     }
-    public void printPerson (){
+
+    public void printPerson() {
         System.out.println("Iм'я: " + name + ", Вik: " + age + ", Професiя: " + job);
     }
+
     public void changeJob(String name, int age, String job) {
         this.job = job;
         System.out.println("Пiсля оновлення професії");
