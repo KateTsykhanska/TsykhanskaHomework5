23b7a6147326fe2fbd4ca1b6f1824a53b2bace8e
Katia <kateeegold+1@gmail.com>
2024-11-25 14:48:05 +0100

    homework6_1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,64 @@
 
-    package org.example;
+package org.example;
 
-    public class Main {
-        public static void main (String[] args) {
-            Homework5_1 homework5_1 = new Homework5_1();
-            homework5_1.checkAge(9);
-            homework5_1.checkAge(10);
-            homework5_1.checkAge(0);
-            homework5_1.checkAge(99);
-            homework5_1.checkAge(35);
-            homework5_1.checkAge(25);
-
-            System.out.println("Homework.isPerfectSquare (25): "+homework5_1.isPerfectSquare(25));
-            System.out.println("Homework.isPerfectSquare (24): "+homework5_1.isPerfectSquare(24));
-
+public class Main {
+    public static int average(int[] nums) {
+        int sum = 0;
+        int average = 0;
+        ;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            average = sum / nums.length;
         }
+        return average;
+    }
+
+    public static int min(int[] nums) {
+        int min = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            min = Math.min(min, nums[i]);
+        }
+        return min;
+    }
+
+    public static int max(int[] nums) {
+        int max = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            max = Math.max(max, nums[i]);
+        }
+        return max;
+
+    }
+
+    public static void reverse(int[] nums) {
+        for (int i = nums.length - 1; i >= 0; i--) {
+            System.out.println(nums[i]);
+        }
+    }
+
+    public static boolean contains(int[] nums, int target) {
+        for (int num : nums) {
+            if (num == target) {
+                return true;
+            }
+        }
+        return false; 
+    }
+    public static void main(String[] args) {
+        System.out.println("Average: " + average(new int[]{2, 1, 2, 3, 2}));
+        System.out.println("Min: " + min(new int[]{2, 1, 2, 3, 2}));
+        System.out.println("Max: " + max(new int[]{2, 1, 2, 3, 2}));
+        System.out.println("Reverse:");
+        reverse(new int[]{2, 1, 2, 3, 2});
+        int target = 3;
+        int[] nums = new int[]{2, 1, 2, 3, 2};
+        boolean result = contains(nums, target);
+        if (result) {
+            System.out.println("Number " + target + " has been found");
+        } else {
+            System.out.println("Number " + target + " hasn't been found");
+        }
+    }
 }
+
+
