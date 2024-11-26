d91d21a5adbb838e9f679e1eb796d09a4ba6b4f9
Katia <kateeegold+1@gmail.com>
2024-11-26 11:43:03 +0100

    homework12.1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,44 @@
 
-    package org.example;
+package org.example;
 
-    public class Main {
-        public static void main (String[] args) {
-            Homework5_1 homework5_1 = new Homework5_1();
-            homework5_1.checkAge(9);
-            homework5_1.checkAge(10);
-            homework5_1.checkAge(0);
-            homework5_1.checkAge(99);
-            homework5_1.checkAge(35);
-            homework5_1.checkAge(25);
+import org.w3c.dom.ls.LSOutput;
 
-            System.out.println("Homework.isPerfectSquare (25): "+homework5_1.isPerfectSquare(25));
-            System.out.println("Homework.isPerfectSquare (24): "+homework5_1.isPerfectSquare(24));
+import java.sql.Array;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Integer> myList = Arrays.asList(3, 14, 15, 92, 6);
+        System.out.println(myList);
+
+
+        List<String> myList1 = new ArrayList<String>() {
+            {
+                add("Kateryna");
+                add("Oleksandrivna");
+                add("Tsykhanska");
+            }
+        };
+        System.out.println(myList1);
+
+        Set<Integer> noDuplicates = new HashSet<>();
+        noDuplicates.add(1);
+        noDuplicates.add(2);
+        noDuplicates.add(3);
+        noDuplicates.add(1);
+        noDuplicates.add(2);
+        System.out.println(noDuplicates);
+
+        Map<String, String> cityPhoneCodes = new HashMap<>();
+        System.out.println("Міста України та їх телефонний код:");
+        cityPhoneCodes.put("Одеса", "048");
+        cityPhoneCodes.put("Харкiв", "057");
+        cityPhoneCodes.put("Днiпро", "056");
+        for (Map.Entry<String, String> entry : cityPhoneCodes.entrySet()) {
+            System.out.println(entry.getKey() + "-" + entry.getValue());
 
         }
+    }
 }
