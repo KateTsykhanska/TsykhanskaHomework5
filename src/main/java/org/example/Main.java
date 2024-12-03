c9602fbbb842e3bbc66eab0303ac6c6c9866648a
Katia <kateeegold+1@gmail.com>
2024-12-03 13:07:21 +0100

    homework13.4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,24 @@
 
-    package org.example;
+package org.example;
 
-    public class Main {
-        public static void main (String[] args) {
+import java.util.HashMap;
+import java.util.Map;
 
-
+public class Main {
+    public static Map<String, String> mergeStringsByFirstChar(String[] strings) {
+        Map<String, String> result = new HashMap<>();
+        for (String str : strings) {
+            String key = str.substring(0, 1);
+            result.put(key, result.getOrDefault(key, "") + str);
         }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println(mergeStringsByFirstChar(new String[]{"salt", "tea", "soda", "toast"}));
+        System.out.println(mergeStringsByFirstChar(new String[]{"aa", "bb", "cc", "aAA", "cCC", "d"}));
+        System.out.println(mergeStringsByFirstChar(new String[]{}));
+    }
 }
