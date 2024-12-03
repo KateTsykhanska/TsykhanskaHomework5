0222d823bdf162bf556b778a5ba2c5bf28cf10ee
Katia <kateeegold+1@gmail.com>
2024-12-03 15:06:00 +0100

    homework13.5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,28 @@
 
-    package org.example;
+package org.example;
 
-    public class Main {
-        public static void main (String[] args) {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Main {
+    public static String buildResultString(String[] strings) {
+        Map<String, Integer> countMap = new HashMap<>();
 
+        String result = "";
+        for (String str : strings) {
+            countMap.put(str, countMap.getOrDefault(str, 0) + 1);
+            if (countMap.get(str) % 2 == 0) {
+                result = str;
+            }
         }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // Тестові приклади
+        System.out.println(buildResultString(new String[]{"a", "b", "a"}));
+        System.out.println(buildResultString(new String[]{"a", "b", "a", "c", "a", "d", "a"}));
+        System.out.println(buildResultString(new String[]{"a", "", "a"}));
+    }
 }
