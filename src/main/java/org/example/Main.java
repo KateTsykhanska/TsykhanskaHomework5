cd17753d99950925e339298abedccf56e3a85740
Katia <kateeegold+1@gmail.com>
2024-12-03 09:14:55 +0100

    homework13.3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,22 @@
 
     package org.example;
 
+    import java.util.HashMap;
+    import java.util.Map;
+
     public class Main {
-        public static void main (String[] args) {
+            public static Map<String, Integer> countWords(String[] words) {
+                Map<String, Integer> wordMap = new HashMap<>();
+                for (String word : words) {
+                    wordMap.put(word, wordMap.getOrDefault(word, 0) + 1);
+                }
+                return wordMap;
+            }
 
+        public static void main(String[] args) {
 
+            System.out.println(countWords(new String[] {"a", "b", "a", "c", "b"}));
+            System.out.println(countWords(new String[] {"c", "b", "a"}));
+            System.out.println(countWords(new String[] {"c", "c", "c", "c"}));
         }
-}
+    }
