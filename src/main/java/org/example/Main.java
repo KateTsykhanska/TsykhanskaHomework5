d3d620c83d0e15209e285f9ffe0fb122d86c2f5c
Katia <kateeegold+1@gmail.com>
2024-12-04 15:45:35 +0100

    homework13.7 git status

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,30 @@
 
     package org.example;
 
+    import java.util.ArrayList;
+    import java.util.HashMap;
+    import java.util.LinkedHashSet;
+    import java.util.Map;
+
     public class Main {
-        public static void main (String[] args) {
+            public static boolean haveCommonElements(LinkedHashSet<Integer> setA, LinkedHashSet<Integer> setB) {
+                LinkedHashSet<Integer> copyA = new LinkedHashSet<>(setA);
+                copyA.retainAll(setB);
+                return !copyA.isEmpty();
+            }
 
+            public static void main(String[] args) {
+                LinkedHashSet<Integer> setA = new LinkedHashSet<>();
+                setA.add(1);
+                setA.add(2);
+                setA.add(3);
+                setA.add(4);
 
+                LinkedHashSet<Integer> setB = new LinkedHashSet<>();
+                setB.add(3);
+                setB.add(5);
+                setB.add(6);
+
+                System.out.println(haveCommonElements(setA, setB));
+            }
         }
-}
