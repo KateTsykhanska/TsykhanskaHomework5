869be1bbd54e91a4a1e36ea98979d1742d8d15ba
Katia <kateeegold+1@gmail.com>
2024-12-06 15:23:23 +0100

    homework7.3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,20 @@
     package org.example;
 
     public class Main {
-        public static void main (String[] args) {
+        public static boolean xyzMiddle(String str) {
+            int len = str.length();
+            if (len < 3) return false;
+            int midStart = (len - 3) / 2;
+            int midEnd = midStart + 1;
+            return (str.substring(midStart, midStart + 3).equals("xyz")) ||
+                    (midEnd < len - 2 && str.substring(midEnd, midEnd + 3).equals("xyz"));
+        }
+
+        public static void main(String[] args) {
+            System.out.println(xyzMiddle("AAxyzBB"));
+            System.out.println(xyzMiddle("AxyzBB"));
+            System.out.println(xyzMiddle("AxyzBBB"));
 
 
         }
-}
+    }
