fe270d1aa8bc05f71630f23ea7362ec3b35d2c66
Katia <kateeegold+1@gmail.com>
2024-12-06 14:21:53 +0100

    homework13.8

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,19 @@
 
-    package org.example;
+package org.example;
 
-    public class Main {
-        public static void main (String[] args) {
+import java.util.Arrays;
+import java.util.TreeSet;
 
+public class Main {
+    public static void main(String[] args) {
+        TreeSet<Integer> setA = new TreeSet<>(Arrays.asList(1, 2, 3));
+        TreeSet<Integer> setB = new TreeSet<>(Arrays.asList(3, 4, 5));
 
-        }
+        TreeSet<Integer> resultSet = new TreeSet<>();
+
+        resultSet.addAll(setA);
+        resultSet.addAll(setB);
+
+        System.out.println("Об'єднаний TreeSet: " + resultSet);
+    }
 }
